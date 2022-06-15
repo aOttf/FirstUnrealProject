35d51bd0757037819023915ad8ff5956c4e848d7
Tyrael <jingyu.wang@mail.mcgill.ca>
2022-06-15 18:55:19 -0400

    Project SetUp

---
+++ Source/ActionGamePrototype/ActionGamePrototypeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionGamePrototypeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONGAMEPROTOTYPE_API AActionGamePrototypeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
